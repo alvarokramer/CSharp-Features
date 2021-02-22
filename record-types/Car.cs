6823c455d2ab39debaf0355fe2fca3446940ddd6
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-02-22 16:49:11 -0300

    record-types project

---
+++ record-types/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace record_types
+{
+    public record Car
+    {
+        public Car(string color, string model, int horsepower)
+        {
+            Color = color;
+            Model = model;
+            Horsepower = horsepower;
+        }
+
+        public string Color { get; }
+
+        public string Model { get; }
+
+        public int Horsepower { get; }
+    }
+
+    //public record Car(string Color, string Model, int Horsepower);
+}
---
+++ record-types/Program.cs
@@ -0,0 +1,12 @@
+﻿using record_types;
+using System;
+
+var modelS = new Car("Red", "ModelS", 250);
+Console.WriteLine(modelS);
+var anotherModelS = new Car("Red", "ModelS", 250);
+Console.WriteLine(anotherModelS);
+
+Console.WriteLine($"Equal? {modelS == anotherModelS}"); // true
+
+//var blueCar = modelS with { Color = "Blue" };
+//Console.WriteLine(blueCar);
